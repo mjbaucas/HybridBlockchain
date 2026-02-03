0a81370d38b94f20d4d138bb6b8368e68c496321
mjbaucas <mjbaucas@gmail.com>
2026-02-03 16:15:17 -0500

    Added versions for hash pow

---
+++ client_hash.py
@@ -0,0 +1,55 @@
+import socket
+import time 
+import sys
+from blockchain.public import Chain as PublicBlockchain
+import json
+
+pub_chain = PublicBlockchain(4)
+
+reset = 1
+start = 0
+total = 0
+counter = 0
+averages = []
+for i in range(3):
+    while counter < 100:
+        if reset == 1:
+            start = time.time()
+            reset = 0
+        try:
+            s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+            s.connect(("192.168.137.1", 5000))
+            #s.sendall(bytes('device001', "utf-8"))
+            s.sendall(bytes('device002', "utf-8"))
+            message = s.recv(1024).decode("utf-8")
+                
+            if message != "":
+                #print(message)
+                message = json.loads(message)
+                print(message["response"]) 
+                if message["response"] == "proof":
+                    proof = pub_chain.proof_of_work(pub_chain.gen_block)
+                    response = proof
+                    s.sendall(bytes(response, "utf-8"))
+                    message = s.recv(1024).decode("utf-8")
+                print(message)
+
+                end = time.time()
+                elapsed = end-start
+                if elapsed > 0:
+                    total += elapsed
+                    counter += 1
+                    print(elapsed)
+                    print(total)
+                    print(counter)
+                    print(total/counter)
+                    if counter == 100:
+                        averages.append(total/counter)
+                reset = 1
+            else:
+                reset = 0
+            s.close()
+        except Exception as msg:
+            print(msg)
+            reset = 0
+print(averages)
--- server_hash.py
+++ server_hash.py
@@ -4,7 +4,6 @@ import sys
 import json
 from blockchain.private import Chain as PrivateBlockchain
 from blockchain.public import Chain as PublicBlockchain
-from sram import read_sram
 
 ip_addresses = [
     "192.168.137.102",
@@ -17,42 +16,34 @@ ip_addresses = [
     #"192.168.137.64"
 ]
 
-sram_data = read_sram('test_sram_data.txt')
-#print(sram_data["100"])
-
-crp_records_private = [
+records_private = [
     {
         "ID": "device001", 
-        "response": "1011x11x", 
-        "sram_address": "100",
-        "row_address": 0,
         "timestamp": "0"
     }
 ]
 
-crp_records_public = [
+records_public = [
     {
         "ID": "device001", 
-        "challenge": ['1011x11x', 'xx1x1110', 'x1x10xxx', 'x0x11x11', '0x0x101x', 'x1xx1x1x', '111x101x', 'x0xx0xxx', '1x1xxxx1', 'x0xxxxxx', 'xxxxx010', 'xx1x1x11', 'x1x1x110', 'xx1x1111', 'x1xxxx1x', 'x10x111x', '01x01x1x', '1x1x000x', 'x11xxxx0', '1xx11xxx', '1xxx111x', 'x1xx1x0x', '011x1xxx', 'xx1xxxx0', '01xx00x0', '111x1xxx', 'x0xx10x0', 'x101x00x', '0xx1xx11', '101xx10x', 'xx1xxx1x', 'xxx10x1x'], 
-        "sram_address": "100",
         "timestamp": "0"
     }
 ]
 
 #intialize private blockchain
-private_trusted_list = crp_records_private.copy()
+private_trusted_list = records_private.copy()
 
 priv_chain = PrivateBlockchain()
 priv_chain.gen_next_block("0", private_trusted_list)
 
 #initialize public blockchain
 pub_chain = PublicBlockchain(4)
-public_trusted_list = crp_records_public.copy()
+public_trusted_list = records_public.copy()
 
 pub_chain.gen_next_block("0", public_trusted_list)
 proof = pub_chain.proof_of_work(pub_chain.gen_block)
 
-def verify_account(device_id, priv_chain, pub_chain):
+def verify_account(device_id, priv_chain, pub_chain, proof):
     private_results = priv_chain.search_ledger(device_id)
     public_results = pub_chain.search_ledger(device_id)
 
@@ -60,8 +51,7 @@ def verify_account(device_id, priv_chain, pub_chain):
     found = False
     for private_result in private_results:
         for public_result in public_results:
-            if public_result["sram_address"] == private_result["sram_address"] and public_result["timestamp"] == private_result["timestamp"]:
-                found = public_result["challenge"][private_result["row_address"]] == private_result["response"]
+            found = pub_chain.verify_proof(pub_chain.gen_block, proof)
 
     return found
 
@@ -100,13 +90,11 @@ while True:
                 response = json.dumps({"response": "access granted"})
                 connection.sendall(bytes(str(response), "utf-8"))    
             else: 
-                sram_address = "100"
-                row_address = 1
-                response = json.dumps({"response": "challenge", "row_address": row_address, "challenge": sram_data[sram_address]})
+                response = json.dumps({"response": "proof"})
                 connection.sendall(bytes(str(response), "utf-8"))
                 message = connection.recv(1024)
                 print(message)
-                if sram_data[sram_address][row_address] == message.decode("utf-8"):
+                if pub_chain.verify_proof(pub_chain.gen_block, message.decode("utf-8")):
                     response = ["access granted"]          
                 else:
                     response = ["access rejected"]
@@ -117,13 +105,11 @@ while True:
                 response = json.dumps({"response": "access granted"})
                 connection.sendall(bytes(str(response), "utf-8"))
             else:
-                sram_address = "100"
-                row_address = 1
-                response = json.dumps({"response": "challenge", "row_address": row_address, "challenge": sram_data[sram_address]})
+                response = json.dumps({"response": "proof"})
                 connection.sendall(bytes(str(response), "utf-8"))
                 message = connection.recv(1024)
                 print(message)
-                if sram_data[sram_address][row_address] == message.decode("utf-8"):
+                if pub_chain.verify_proof(pub_chain.gen_block, message.decode("utf-8")):
                     response = ["access granted"]          
                 else:
                     response = ["access rejected"]
